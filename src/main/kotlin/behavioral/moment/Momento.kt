0d3f56938b760a10b86af53d6ba450dd6a976563
yaroslav-android <yaroslav.ovdiienko@gmail.com>
2024-01-31 15:02:23 +0200

    docs: add Momento docs

--- src/main/kotlin/behavioral/moment/Momento.kt
+++ src/main/kotlin/behavioral/moment/Momento.kt
@@ -4,7 +4,15 @@ import team.idivision.samples.designpatterns.Pattern
 import team.idivision.samples.designpatterns.behavioral.moment.editor.TextEditor
 import team.idivision.samples.designpatterns.behavioral.moment.editor.UndoManager
 
-
+/** Use the Memento pattern when
+ * Without violating encapsulation, capture and externalize an object's internal state
+ * so that the object can be restored to this state later.
+ *
+ * - a snapshot of (some portion of) an object's state must be saved so that it
+ *      can be restored to that state later, and
+ * - a direct interface to obtaining the state would expose implementation
+ *      details and break the object's encapsulation.
+ */
 class Momento : Pattern("Momento") {
     override fun launch() {
         val textEditor = TextEditor()
